c08d1975f3629aa84f18b6e0a5e7a0cfb59e172a
Xavid <xavid@mit.edu>
2015-06-30 05:42:02 -0400

    Initial plugin version.

---
+++ index.js
@@ -0,0 +1,19 @@
+exports.eejsBlock_styles = function(hook_name, args, cb){
+   args.content += '<style type="text/css">' 
+    + '.toolbar ul li.syncstatus {'
+    + '  display: none;'
+    + '  line-height: 32px;'
+    + '  margin-left: 10px;'
+    + '  font-size: 14pt;'
+    + '  color: #666;'
+    + '}'
+    + "</style>";
+  cb();
+}
+
+exports.eejsBlock_editbarMenuLeft = function (hook_name, context, cb) {
+  context.content
+    += '<li class="syncstatus" id="syncstatussyncing">Saving...</li>'
+    + '<li class="syncstatus" id="syncstatusdone">Saved.</li>';
+  cb();
+};

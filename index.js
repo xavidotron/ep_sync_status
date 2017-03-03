3e40d51099ab943f0deb05cdba219cdef1bf181a
Luiza Pagliari <lpagliari@gmail.com>
2017-03-03 18:57:00 -0300

    [refactor] Extract CSS and HTML into templates
    Follow Etherpad recomendation to use EJS for HTML elements added:
    http://etherpad.org/doc/v1.6.0/#index_templates.

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
+var eejs = require('ep_etherpad-lite/node/eejs/');
+
 exports.eejsBlock_styles = function(hook_name, context, cb){
-   context.content += '<style type="text/css">'
-    + '.toolbar ul li.syncstatus:not(.separator) {'
-    + '  display: none;'
-    + '  line-height: 32px;'
-    + '  font-size: 14pt;'
-    + '  color: #666;'
-    + '}'
-    + '</style>';
+  context.content += '<link href="../static/plugins/ep_sync_status/static/css/editbar.css" rel="stylesheet">';
   cb();
 }
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, context, cb) {
-  context.content
-    += '<li class="syncstatus separator acl-write"></li>'
-    +  '<li class="syncstatus" id="syncstatussyncing" data-l10n-id="ep_sync_status.saving">Saving...</li>'
-    +  '<li class="syncstatus" id="syncstatusdone"    data-l10n-id="ep_sync_status.saved">Saved.</li>';
+  context.content += eejs.require('ep_sync_status/templates/syncStatus.ejs');
   cb();
 };

8d2ee47589ba6488f01f42f8a1fce4ab41dde1c8
Luiza Pagliari <lpagliari@gmail.com>
2017-03-03 18:43:10 -0300

    [style] Show status messages at the right of last item on menu left
    Other plugins might add more items to menu left, so avoid moving them
    every time the sync status is changed.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 exports.eejsBlock_styles = function(hook_name, context, cb){
    context.content += '<style type="text/css">'
-    + '.toolbar ul li.syncstatus {'
+    + '.toolbar ul li.syncstatus:not(.separator) {'
     + '  display: none;'
     + '  line-height: 32px;'
-    + '  margin-left: 10px;'
     + '  font-size: 14pt;'
     + '  color: #666;'
     + '}'
-    + "</style>";
+    + '</style>';
   cb();
 }
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, context, cb) {
   context.content
-    += '<li class="syncstatus" id="syncstatussyncing" data-l10n-id="ep_sync_status.saving">Saving...</li>'
+    += '<li class="syncstatus separator acl-write"></li>'
+    +  '<li class="syncstatus" id="syncstatussyncing" data-l10n-id="ep_sync_status.saving">Saving...</li>'
     +  '<li class="syncstatus" id="syncstatusdone"    data-l10n-id="ep_sync_status.saved">Saved.</li>';
   cb();
 };
